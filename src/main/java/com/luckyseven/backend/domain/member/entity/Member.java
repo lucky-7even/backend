706141a983cd1e5ed4be48d14bce7b007c88da9f
jmk307 <jimin112688@gmail.com>
2022-11-08 02:55:44 +0900

    feat : 환경분리 및 기본 엔티티 추가

---
+++ src/main/java/com/luckyseven/backend/domain/member/entity/Member.java
@@ -0,0 +1,34 @@
+package com.luckyseven.backend.domain.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String nickname;
+
+    private String profileImage;
+
+    private String email;
+
+    private String password;
+
+    // 경도
+    private Double lat;
+
+    // 위도
+    private Double lng;
+}
