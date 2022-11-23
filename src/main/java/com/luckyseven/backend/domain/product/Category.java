282e6673df5ceb0247c49a357af45686419acb3f
hanjongho <lgwl81@gmail.com>
2022-11-24 02:56:25 +0900

    .

--- src/main/java/com/luckyseven/backend/domain/product/Category.java
+++ src/main/java/com/luckyseven/backend/domain/product/Category.java
@@ -2,5 +2,6 @@ package com.luckyseven.backend.domain.product;
 
 public enum Category {
 	// 카테고리 등록
-	AA, BB, CC, DD
+	LIVING, CLOTHES, SPORTS, GAME, BOOK,
+	DIGITAL, BEAUTY, CHILD_PRODUCT, ELECTRONICS, ETC
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO MEMBER VALUES (1L, 'Jongho', 37.123, 127.123,  'test@naver.com', '12345678', 'profileImage');
 
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (1L, 'AA', '설명1', '물건1', 10000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (2L, 'BB', '설명2', '물건2', 20000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (3L, 'CC', '설명3', '물건3', 30000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (4L, 'CC', '설명4', '물건4', 40000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (5L, 'CC', '설명5', '물건5', 50000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (6L, 'CC', '설명6', '물건6', 60000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (7L, 'CC', '설명7', '물건7', 70000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (8L, 'CC', '설명8', '물건8', 80000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (9L, 'CC', '설명9', '물건9', 90000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (10L, 'CC', '설명10', '물건10', 100000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (11L, 'CC', '설명11', '물건11', 110000, 'WAITING', 'BONGCHEON', 1L);
-INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (12L, 'CC', '설명12', '물건12', 120000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (1L, 'LIVING', '설명1', '물건1', 10000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (2L, 'CLOTHES', '설명2', '물건2', 20000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (3L, 'SPORTS', '설명3', '물건3', 30000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (4L, 'SPORTS', '설명4', '물건4', 40000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (5L, 'SPORTS', '설명5', '물건5', 50000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (6L, 'SPORTS', '설명6', '물건6', 60000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (7L, 'SPORTS', '설명7', '물건7', 70000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (8L, 'SPORTS', '설명8', '물건8', 80000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (9L, 'SPORTS', '설명9', '물건9', 90000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (10L, 'SPORTS', '설명10', '물건10', 100000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (11L, 'SPORTS', '설명11', '물건11', 110000, 'WAITING', 'BONGCHEON', 1L);
+INSERT INTO PRODUCT(id, category, name, description, price, product_status, region, member_id) VALUES (12L, 'SPORTS', '설명12', '물건12', 120000, 'WAITING', 'BONGCHEON', 1L);
